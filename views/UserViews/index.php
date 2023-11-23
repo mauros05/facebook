978080192d751ffe59eaef264ad2f3b654f8c80a
Mauricio Gutierrez <mauricio.gutley@gmail.com>
2023-11-22 22:33:42 -0600

    Add padding

--- views/UserViews/index.php
+++ views/UserViews/index.php
@@ -1,24 +1,23 @@
-<div>
-<table class="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First Name</th>
-      <th scope="col">Last Name</th>
-      <th scope="col">Email</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php for($i = 0; $i < count($res["id_user"]); $i++){?>
-    <tr>
-      <th scope="row"><?= $res["id_user"][$i] ?></th>
-      <td><?= $res["first_name"][$i] ?></td>
-      <td><?= $res["middle_name"][$i] ?> <?= $res["last_name"][$i] ?></td>
-      <td><?= $res["email"][$i] ?></td>
-    </tr>
-    <?php }?>
-  </tbody>
-</table>
-
+<div class="p-2">
+    <table class="table">
+    <thead>
+        <tr>
+        <th scope="col">#</th>
+        <th scope="col">First Name</th>
+        <th scope="col">Last Name</th>
+        <th scope="col">Email</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php for($i = 0; $i < count($res["id_user"]); $i++){?>
+            <tr>
+            <th scope="row"><?= $res["id_user"][$i] ?></th>
+            <td><?= $res["first_name"][$i] ?></td>
+            <td><?= $res["middle_name"][$i] ?> <?= $res["last_name"][$i] ?></td>
+            <td><?= $res["email"][$i] ?></td>
+            </tr>
+        <?php }?>
+    </tbody>
+    </table>
 </div>
 
