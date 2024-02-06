98de897f4334894fad8504d5f6d5b4a6965fbb62
Mauricio Gutierrez <mauricio.gutley@gmail.com>
2024-02-05 23:27:33 -0600

    Add Navbar to list post view

--- controllers/PostController.php
+++ controllers/PostController.php
@@ -6,7 +6,10 @@
         }
 
         public function listPosts(){
+            require_once("views/Templates/Header.php");
+            require_once("views/Components/Navbar.php");
             require_once("views/PostViews/index.php");
+            require_once("views/Templates/Footer.php");
         }
     
         public function createPost(){
